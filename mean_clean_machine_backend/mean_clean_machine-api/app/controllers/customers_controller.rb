d14ae45e0f8914f9b114054e7b93ba2e8ca5867c
courserav <lvsmlit@gmail.com>
2021-08-20 12:54:06 -0400

    seeded data to db. made migrations to db. added backend search to customer button.

--- mean_clean_machine_backend/mean_clean_machine-api/app/controllers/customers_controller.rb
+++ mean_clean_machine_backend/mean_clean_machine-api/app/controllers/customers_controller.rb
@@ -1,2 +1,11 @@
 class CustomersController < ApplicationController
+    def index
+        customers = Customer.all
+        render json: customers.to_json
+    end
+
+    def show
+        customer = Customer.find_by(params: [:id])
+        render json: customer.to_json
+    end
 end
--- mean_clean_machine_backend/mean_clean_machine-api/db/seeds.rb
+++ mean_clean_machine_backend/mean_clean_machine-api/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+customer = Customer.create(first_name: "Ricky", last_name: "Spanish", email: "rickyspan1sh@aol.com", phone_number: "555-555-5555")
+customer.save!
