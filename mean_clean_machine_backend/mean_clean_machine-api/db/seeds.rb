a21efe12b86d0fa01110decad304e5841803e108
courserav <lvsmlit@gmail.com>
2021-08-21 05:57:56 -0400

    added more seed data.

--- mean_clean_machine_backend/mean_clean_machine-api/db/seeds.rb
+++ mean_clean_machine_backend/mean_clean_machine-api/db/seeds.rb
@@ -8,3 +8,16 @@
 
 customer = Customer.create(first_name: "Ricky", last_name: "Spanish", email: "rickyspan1sh@aol.com", phone_number: "555-555-5555")
 customer.save!
+
+Customer.create(first_name: "Matilda", last_name: "Woormwood", email: "psychick@yahoo.com", phone_number: "555-555-5555")
+Customer.create(first_name: "", last_name: "Pitoo", email: "secondpit@aol.com", phone_number: "555-555-5555")
+Customer.create(first_name: "", last_name: "Pit", email: "firstpit@aol.com", phone_number: "555-555-5555")
+Customer.create(first_name: "Reggie", last_name: "Rocket", email: "", phone_number: "")
+Customer.create(first_name: "Otto", last_name: "Rocket", email: "", phone_number: "")
+Customer.create(first_name: "Raymundo", last_name: "Rocket", email: "daddyrocket@aol.com", phone_number: "555-555-5555")
+Customer.create(first_name: "Ash", last_name: "Ketchum", email: "", phone_number: "")
+Customer.create(first_name: "Bernard", last_name: "Sanders", email: "icouldhavebeenpresidentifthedemocratsdidntcannabalizeme@whitehouse.gov", phone_number: "")
+Customer.create(first_name: "Donald", last_name: "Garison", email: "yuge@trumpu.edu", phone_number: "")
+Customer.create(first_name: "", last_name: "test", email: "", phone_number: "")
+Customer.create(first_name: "Bubbles", last_name: "n/a", email: "", phone_number: "")
+Customer.create(first_name: "LastPlaceHolder", last_name: "Z", email: "", phone_number: "")
